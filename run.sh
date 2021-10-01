f603a860249154cdf8d71d428a50ca90f974de10
Hayden <reoccuringcat@protonmail.com>
2021-10-01 13:49:50 -0500

    prompt for killing process and improvements

--- run.sh
+++ run.sh
@@ -1,9 +1,13 @@
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 BLUE='\033[0;34m'
+LBLUE='\033[1;34m'
+YELLOW='\033[1;33m'
+PURPLE='\033[1;35m'
 NC='\033[0m'
-printf "${BLUE}                _      _           _         _    _            \n  _  _ _ __  __| |__ _| |_ ___ ___(_)_ _  __(_)__| |___ _ _ ___\n | || | '_ \/ _\` / _\` |  _/ -_)___| | ' \(_-< / _\` / -_) '_(_-<\n  \_,_| .__/\__,_\__,_|\__\___|   |_|_||_/__/_\__,_\___|_| /__/\n      |_|        \n\n${NC}"
-printf "Getting ${RED}SUDO${NC} access...\n"
+printf "${PURPLE}                _      _           _         _    _            \n  _  _ _ __  __| |__ _| |_ ___ ___(_)_ _  __(_)__| |___ _ _ ___\n | || | '_ \/ _\` / _\` |  _/ -_)___| | ' \(_-< / _\` / -_) '_(_-<\n  \_,_| .__/\__,_\__,_|\__\___|   |_|_||_/__/_\__,_\___|_| /__/\n      |_|        \n${NC}"
+printf '${LBLUE}(C) reoccurcat 2021-present using the GNU GPL v3 License${NC}\n\n'
+printf "Getting ${YELLOW}SUDO${NC} access...\n"
 sudo printf "${GREEN}Achieved!${NC}\n"
 if [ $? -gt 0 ]; then
     echo -e "${RED}Failed. Please rerun the script.${NC}"
@@ -12,18 +16,51 @@ fi
 echo "$ pgrep -f 'code-insiders' >> /dev/null"
 pgrep -f 'code-insiders' >> /dev/null
 if [ $? -eq 0 ]; then
-    echo -e "${BLUE}Insider instances running, killing...${NC}"
-    echo "$ pkill -e -f 'code-insiders'"
-    pkill -e -f 'code-insiders'
-    if [ $? -gt 0 ]; then
+    echo -e "${YELLOW}Insider instances are running.${NC}"
+    while true
+    do
+      read -r -p "Do you want to kill them? [Y/n] " input
+      case $input in
+        [yY][eE][sS]|[yY])
+      echo "$ pkill -e -f 'code-insiders'"
+      pkill -e -f 'code-insiders'
+      if [ $? -gt 0 ]; then
         echo -e "${RED}Failed to kill processes. ATTEMTING AS ROOT.${NC}"
         echo "# pkill -e -f 'code-insiders'"
         sudo pkill -e -f 'code-insiders'
         if [ $? -gt 0 ]; then
-            echo -e "${RED}Failed.${NC}"
-            exit
+          echo -e "${RED}Failed.${NC}"
+          exit
         fi
-    fi
+      fi
+      break
+      ;;
+        [nN][oO]|[nN])
+      echo -e "${RED}CONTINUE AT YOUR OWN RISK. FILE CORRUPTION IS POSSIBLE.${NC}"
+        while true
+        do
+          read -r -p "Are you sure you want to continue? [Y/n] " input
+          case $input in
+            [yY][eE][sS]|[yY])
+          echo -e "${RED}YOU HAVE BEEN WARNED.${NC}"
+          break
+          ;;
+            [nN][oO]|[nN])
+          echo -e "${BLUE}Exiting...${NC}"
+          exit
+                 ;;
+              *)
+          echo -e "${YELLOW}Invalid input.${NC}"
+          ;;
+          esac
+        done
+        break
+                ;;
+             *)
+        echo -e "${YELLOW}Invalid input.${NC}"
+          ;;
+          esac
+        done
 else
     echo -e "${BLUE}No insider instances running, continuing...${NC}"
 fi
@@ -46,5 +83,17 @@ if [[ -f "/usr/share/applications/code-insiders.desktop" ]]; then
 fi
 echo "# cp ./code-insiders.desktop /usr/share/applications/code-insiders.desktop"
 sudo cp ./code-insiders.desktop /usr/share/applications/code-insiders.desktop
-printf "${GREEN}All done! Check your application menu!${NC}\n"
+echo '$ grep -Fxq "alias code-insiders=/usr/share/vscode-insiders/bin/code-insiders" ~/.bashrc'
+grep -Fxq "alias code-insiders=/usr/share/vscode-insiders/bin/code-insiders" ~/.bashrc
+if [ $? -eq 1 ]; then
+echo -e "${BLUE}Alias not created, creating now...${NC}"
+echo "$ echo 'alias code-insiders=/usr/share/vscode-insiders/bin/code-insiders' >> ~/.bashrc"
+echo 'alias code-insiders=/usr/share/vscode-insiders/bin/code-insiders' >> ~/.bashrc
+echo '$ source ~/.bashrc'
+source ~/.bashrc
+else
+echo -e "${BLUE}Alias already found, skipping...${NC}"
+fi
+printf "${GREEN}All done! Check your application menu! You can also run 'code-insiders' to start it.${NC}\n"
 exit
+
